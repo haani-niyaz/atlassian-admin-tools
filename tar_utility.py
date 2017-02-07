cfc5e6e5afee650c76b0fc54473a447ba4773390
Haani Niyaz <haani.niyaz@gmail.com>
2017-02-07 20:56:52 +1100

    Add log messages to create tar file function

--- tar_utility.py
+++ tar_utility.py
@@ -14,18 +14,19 @@ Create tar files from:
 '''
 
 
-def create_tar(tarfile_dest, root_dir, tar_dir):
+def create_tar(tarfile_dest, root_dir, tar_file):
 	'''
 	Create a tar file
 	'''
 	
+	logging.info('Creating tar file for {} in root dir {} and stre in {}'.format(tar_file, root_dir, tarfile_dest))		
 	with tarfile.open(tarfile_dest, 'w:gz') as tar:
 		os.chdir(root_dir)
 		try:
-			tar.add(tar_dir)	
+			tar.add(tar_file)	
 		except OSError as e:
+			logging.error('Tar file creation faile with error: ' + str(e))
 			print e
-
 
 
 
@@ -33,6 +34,8 @@ if __name__ == '__main__':
 
 	logging.basicConfig(filename='tar_events.log', level=logging.DEBUG)
 
+	# Test with fake directory
+	# create_tar('/tmp/jira-installdir.tar.gz','/opt/atlassian','bogus')
 
 
 
