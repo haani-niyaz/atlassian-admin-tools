0f0c7ea893c86c529486bd7475c7e556135c6eed
Haani Niyaz <haani.niyaz@gmail.com>
2017-02-08 21:00:41 +1100

    Enable logging to stdout and log file

--- tar_utility.py
+++ tar_utility.py
@@ -1,9 +1,12 @@
 #! /usr/bin/env python
 
-import tarfile, os, logging
+import tarfile, os
+import logging
 
 
-
+def log():
+	logger = logging.getLogger('atlassian-admin-tools')
+	return logger
 
 
 def check_if_exists(tarfile_dest):
@@ -25,18 +28,20 @@ def create_tar(tarfile_dest, root_dir, tar_file):
 
 	if check_if_exists(tarfile_dest):
 		log_str = '{} already exists'.format(tarfile_dest)
-		logging.info(log_str)
-		print log_str
+		log().warn(log_str)
+		# print log_str
 		return None
 
 	
-	logging.info('Creating tar file for {} in root dir {} and stre in {}'.format(tar_file, root_dir, tarfile_dest))		
+	log().info('Creating tar file for {} in root dir {} and store in {}'.format(tar_file, root_dir, tarfile_dest))		
 	with tarfile.open(tarfile_dest, 'w:gz') as tar:
 		os.chdir(root_dir)
 		try:
-			tar.add(tar_file)	
+			tar.add(tar_file)
+			log.info('Tar file creation successful')		
 		except OSError as e:
-			logging.error('Tar file creation faile with error: ' + str(e))
+			log().error('Tar file creation failed with error: ' + str(e))
+			log()
 			print e
 
 
@@ -47,7 +52,7 @@ def extract_tar(root_dir, tar_file):
 	'''
 
 	try:
-		logging.info('Extract tar file {} into {} directory'.format(tar_file, root_dir))	
+		log().info('Extract tar file {} into {} directory'.format(tar_file, root_dir))	
 		tar = tarfile.open(tar_file)
 		tar.extractall(path=root_dir)
 		tar.close()
@@ -57,18 +62,15 @@ def extract_tar(root_dir, tar_file):
 		print e
 
 
-
 if __name__ == '__main__':
-
-	logging.basicConfig(filename='tar_events.log', level=logging.DEBUG)
 
 	# Test with fake directory
 	# create_tar('/tmp/jira-installdir.tar.gz','/opt/atlassian','bogus')
 
-	# create_tar('/tmp/jira-installdir.tar.gz','/opt/atlassian','jira')
+	tar_utility.create_tar('/tmp/jira-installdir.tar.gz','/opt/atlassian','jira')
 
+	# extract_tar('/var/tmp', '/tmp/jira-installdir.tar.gz')
 
-	extract_tar('/var/tmp', '/tmp/jira-installdir.tar.gz')
 
 
 
