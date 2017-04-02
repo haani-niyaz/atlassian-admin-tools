a1000b4394629178087f9299bba86c63063fbc1a
Haani Niyaz <haani.niyaz@gmail.com>
2017-04-02 16:58:29 +1000

    Download artefacts

--- admin_tasks.py
+++ admin_tasks.py
@@ -21,12 +21,41 @@ def make_dirs(dirs):
 
 	return False
 
+
 def set_ownership(path,user='proteus'):
 	uid = pwd.getpwnam(user).pw_uid
 	gid = pwd.getpwnam(user).pw_gid
 
 	log.info("Setting %s ownership to %s:%s " % (path,user,user))
 	os.chown(path,uid,gid)
+
+
+def get_filename(url):
+	return os.path.basename(url)
+
+
+def download(url,path):
+	
+	os.chdir(path)	
+	file_name = get_filename(url)
+	if not os.path.exists(file_name):
+		try:		
+			resp = urllib2.urlopen(url)
+
+			try:
+				fh = open(file_name,'wb')
+				fh.write(resp.read())
+				log.info("Downloaded %s to %s", file_name, path)
+				return True
+			finally:
+				fh.close()
+		except (urllib2.URLError, urllib2.HTTPError), e:
+			log.error("Failed to download %s with error: %s" % (url,str(e)))
+
+	else:
+		log.warn("%s already exists." % file_name)
+
+	return False
 
 
 if __name__ == '__main__':
