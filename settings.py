8c4524f722ee81614bf632710ed3f8aded500daf
Haani Niyaz <haani.niyaz@gmail.com>
2017-04-04 13:06:19 +1000

    Error handling for invalid file name

--- settings.py
+++ settings.py
@@ -1,15 +1,21 @@
 #! /usr/bin/env python
 import simplejson as json
-
+import os
+import sys
 
 def get_config(file):
-	# Nesting required to use finally prior python 2.5
-	try:
+	if os.path.exists(file):
+		# Nesting required to use finally prior python 2.5
 		try:
-			fh = open(file ,'r')
-			config = json.load(fh)
-			return config
-		except IOError, e:
-			log.error('Unable to read yaml file: ' + str(e))
-	finally:
-		fh.close()
+			try:
+				fh = open(file ,'r')
+				config = json.load(fh)
+				return config
+			except IOError, e:
+				log.error('Unable to read yaml file: ' + str(e))
+		finally:
+			fh.close()
+
+	else:
+		print('Please ensure config file path is valid')
+		sys.exit(1)		
