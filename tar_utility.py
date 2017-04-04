ccf562a4f9ec62e10cb4f5e7a6f9838fc2487ca8
Haani Niyaz <haani.niyaz@gmail.com>
2017-04-04 11:20:45 +1000

    Close tar file handler

--- tar_utility.py
+++ tar_utility.py
@@ -14,7 +14,6 @@ log = logging.getLogger('atlassian-admin-tools')
 def check_if_exists(tarfile_dest):
     return os.path.isfile(tarfile_dest)
 
-
 def create_tar(dest_file, parent_dir, backup_dir):
     '''
     Create tar file
@@ -34,31 +33,33 @@ def create_tar(dest_file, parent_dir, backup_dir):
     os.chdir(parent_dir)    
 
     try:
-        tar = tarfile.open(dest_file, 'w:gz')
-        tar.add(backup_dir)
-        log.info("%s creation successful" % dest_file)
-    except OSError, e:
-        log.error('Tar file creation failed with error: ' + str(e))
-        print e
-
-
-def extract_tar(dest_dir, tar_file):
-    '''
-    Extract tar file inside dest dir
-    '''
-    try:
-        log.info("Extract tar file %s into %s directory".format(tar_file, dest_dir))
-        tar = tarfile.open(tar_file,'r:gz')
-        for item in tar:
-            print item
-            tar.extract(item, path=dest_dir)
+        try:
+            tar = tarfile.open(dest_file, 'w:gz')
+            tar.add(backup_dir)
+            log.info("%s creation successful" % dest_file)
+        except OSError, e:
+            log.error('Tar file creation failed with error: ' + str(e))
+            print e
+    finally:
         tar.close()
-    except OSError, e:
-        print e
-        log.error('Tar file extraction failed with error: ' + str(e))
-    except IOError, e:
-        print e
-        log.error('Tar file extraction failed with error: ' + str(e))
+
+# def extract_tar(dest_dir, tar_file):
+#     '''
+#     Extract tar file inside dest dir
+#     '''
+#     try:
+#         log.info("Extract tar file %s into %s directory".format(tar_file, dest_dir))
+#         tar = tarfile.open(tar_file,'r:gz')
+#         for item in tar:
+#             print item
+#             tar.extract(item, path=dest_dir)
+#         tar.close()
+#     except OSError, e:
+#         print e
+#         log.error('Tar file extraction failed with error: ' + str(e))
+#     except IOError, e:
+#         print e
+#         log.error('Tar file extraction failed with error: ' + str(e))
 
 
 if __name__ == '__main__':
