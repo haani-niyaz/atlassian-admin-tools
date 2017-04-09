d55d582cbb17a7ea72ec30e41f04ff0781c32d8a
Haani Niyaz <haani.niyaz@gmail.com>
2017-04-09 20:01:03 +1000

    Fix PEP8 errors. Closes #12.

--- backup.py
+++ backup.py
@@ -5,8 +5,9 @@ import tar_utility
 
 class Backup(object):
 
-    def __init__(self,config,log):
-        self.backup_working_dir = config['base_backup_dir'] +'/'+ config['CRQ']
+    def __init__(self, config, log):
+        self.backup_working_dir = config[
+            'base_backup_dir'] + '/' + config['CRQ']
         self.temp_dir           = config['temp_dir']
         self.backup_dirs        = config['backup']['dirs']
         self.backup_files       = config['backup']['files']
@@ -17,40 +18,46 @@ class Backup(object):
 
     def create_backup_dir(self):
         if admin_tasks.make_dirs(self.backup_working_dir):
-            admin_tasks.set_ownership(self.backup_working_dir)  
+            admin_tasks.set_ownership(self.backup_working_dir)
 
     def backup_app(self):
-        
+
         for index, backup_metadata in enumerate(self.backup_dirs):
             for data in backup_metadata.itervalues():
-                self.log.info("Backing up directory %s of %s.." % (index+1,len(self.backup_dirs)))
-                
+                self.log.info("Backing up directory %s of %s.." %
+                              (index+1, len(self.backup_dirs)))
+
                 dest_file = self.backup_working_dir + '/' + data['tar_file']
                 tar_utility.create_tar(
                     dest_file,
                     data['parent_dir'],
                     data['backup_dir'])
-                admin_tasks.set_permissions(dest_file,0400)
-                self.files_backed_up.append(dest_file)
 
-                if self.log: self.log.info("File Details: %s " % admin_tasks.get_file_details(dest_file))       
+                admin_tasks.set_permissions(dest_file, 0400)
+                # Create a list to output summary
+                self.files_backed_up.append(dest_file)
+                self.log.info("File Details: %s " %
+                              admin_tasks.get_file_details(dest_file))
 
     def backup_config(self):
-        for index,files_metadata in enumerate(self.backup_files):
-             for file_name,source_path in files_metadata.iteritems():
+        for index, files_metadata in enumerate(self.backup_files):
+            for file_name, source_path in files_metadata.iteritems():
 
                 dest_file = self.backup_working_dir + '/' + file_name
-                self.log.info("Backing up config file %s of %s.." % (index+1,len(self.backup_files)))      
-                if admin_tasks.copy_file(source_path,dest_file):
+                self.log.info("Backing up config file %s of %s.." %
+                              (index+1, len(self.backup_files)))
+
+                if admin_tasks.copy_file(source_path, dest_file):
                     self.log.info("Backup of %s is done" % dest_file)
 
-                admin_tasks.set_permissions(dest_file,0400)    
+                admin_tasks.set_permissions(dest_file, 0400)
+                # Create a list to output summary
                 self.files_backed_up.append(dest_file)
-                self.log.info("File Details: %s " % admin_tasks.get_file_details(dest_file))
+                self.log.info("File Details: %s " %
+                              admin_tasks.get_file_details(dest_file))
 
     def summary(self):
         self.log.debug("-- Backup Summary --")
-
         for file_path in self.files_backed_up:
             self.log.info(admin_tasks.get_file_details(file_path))
 
