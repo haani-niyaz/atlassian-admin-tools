50a8db0ffc39bfa355e5762471ae79aa84dd1861
Haani Niyaz <haani.niyaz@gmail.com>
2017-04-16 17:24:50 +1000

    Use main function in module

--- proteusadmintools/multi_logging.py
+++ proteusadmintools/multi_logging.py
@@ -1,39 +1,36 @@
 #! /usr/bin/env python
 
-'''
+"""
 Logging to file and standard output
-'''
+"""
 
 import logging
 
 
-logger = logging.getLogger('atlassian-admin-tools')
-logger.setLevel(logging.DEBUG)
+def main():
 
-# Create a file handler which logs DEBUG messages
-file_handler = logging.FileHandler('/var/tmp/atlassian_admin_tools.log')
-file_handler.setLevel(logging.DEBUG)
+    logger = logging.getLogger('atlassian-admin-tools')
+    logger.setLevel(logging.DEBUG)
 
-# Create a console handler with a higher log level
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
+    # Create a file handler which logs DEBUG messages
+    file_handler = logging.FileHandler('/var/tmp/atlassian_admin_tools.log')
+    file_handler.setLevel(logging.DEBUG)
 
-# Create a formatter
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    # Create a console handler with a higher log level
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
 
-# Add formatter to handlers
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
+    # Create a formatter
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-# add the handlers to logger
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+    # Add formatter to handlers
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    # add the handlers to logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
 
 if __name__ == '__main__':
-
-    logger.debug('debug message')
-    logger.info('info message')
-    logger.warn('warn message')
-    logger.error('error message')
-    logger.critical('critical message')
+    main()
