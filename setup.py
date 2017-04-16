e7a1c8a29c32f4083de1cd4d3d40975da65d7c22
Haani Niyaz <haani.niyaz@gmail.com>
2017-04-16 18:27:43 +1000

    Refer to modules as python packages

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 #! /usr/bin/env python
 
-# Append to module path
-sys.path.append(os.path.join(
-    os.path.dirname(sys.path[0]), 'utils'))
-
 import cmd_options
 import logging
 import multi_logging
-import admin_tasks
+from utils import admin_tasks
 import settings
-from backup import Backup
-from download import Download
-from process import Process
+from controllers import backup as bkp
+from controllers import download as dl
+from controllers import process
 
 
 def main():
@@ -39,11 +35,11 @@ def main():
             # Backup requires shutdown option
             if options.backup and options.shutdown:
 
-                backup = Backup(config, log)
+                backup = bkp.BackupController(config, log)
                 backup.create_backup_dir()
                 log.debug("Backup working directory is %s" %
                           backup.backup_working_dir)
-                is_shutdown = Process(log, app_name).shutdown()
+                is_shutdown = process.ProcessController(log, app_name).shutdown()
                 if is_shutdown:
                     # Drop privileges to 'proteus' user
                     admin_tasks.change_user()
@@ -53,7 +49,7 @@ def main():
                     backup.summary()
 
             elif options.download:
-                download = Download(config, log)
+                download = dl.DownloadController(config, log)
                 download.download_files()
                 download.summary()
 
@@ -62,10 +58,10 @@ def main():
 
         # Options can run without config file
         elif options.process:
-            Process(log, app_name).get_process()
+            process.ProcessController(log, app_name).get_process()
 
         elif options.shutdown:
-            Process(log, app_name).shutdown()
+            process.ProcessController(log, app_name).shutdown()
 
         # Show help if app name and config file has been provided but no
         # switch
@@ -73,11 +69,11 @@ def main():
             parser.print_help()
 
     elif options.disk_space:
-        Process(log).check_disk_space(options.disk_space)
+        process.ProcessController(log).check_disk_space(options.disk_space)
 
     elif options.package_name:
         if args:
-            Process(log).package_info(options.package_name, args[0])
+            process.ProcessController(log).package_info(options.package_name, args[0])
         else:
             parser.print_help()
 
