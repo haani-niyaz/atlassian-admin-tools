e7d80248a1b7b73db0ace0ff653ec840e821fe21
Haani Niyaz <haani.niyaz@gmail.com>
2017-04-17 11:46:13 +1000

    Better import statements

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from core import cmd_options
 from core import multi_logging
 from core import settings
 from utils import admin_tasks
-from controllers import backup as bkp
-from controllers import download as dl
-from controllers import process
+from controllers.backup import BackupController
+from controllers.download import DownloadController
+from controllers.process import ProcessController
 
 
 def main():
@@ -35,11 +35,11 @@ def main():
             # Backup requires shutdown option
             if options.backup and options.shutdown:
 
-                backup = bkp.BackupController(config, log)
+                backup = BackupController(config, log)
                 backup.create_backup_dir()
                 log.debug("Backup working directory is %s" %
                           backup.backup_working_dir)
-                is_shutdown = process.ProcessController(
+                is_shutdown = ProcessController(
                     log, app_name).shutdown()
                 if is_shutdown:
                     # Drop privileges to 'proteus' user
@@ -50,7 +50,7 @@ def main():
                     backup.summary()
 
             elif options.download:
-                download = dl.DownloadController(config, log)
+                download = DownloadController(config, log)
                 download.download_files()
                 download.summary()
 
@@ -59,10 +59,10 @@ def main():
 
         # Options can run without config file
         elif options.process:
-            process.ProcessController(log, app_name).get_process()
+            ProcessController(log, app_name).get_process()
 
         elif options.shutdown:
-            process.ProcessController(log, app_name).shutdown()
+            ProcessController(log, app_name).shutdown()
 
         # Show help if app name and config file has been provided but no
         # switch
@@ -70,11 +70,11 @@ def main():
             parser.print_help()
 
     elif options.disk_space:
-        process.ProcessController(log).check_disk_space(options.disk_space)
+        ProcessController(log).check_disk_space(options.disk_space)
 
     elif options.package_name:
         if args:
-            process.ProcessController(log).package_info(
+            ProcessController(log).package_info(
                 options.package_name, args[0])
         else:
             parser.print_help()
