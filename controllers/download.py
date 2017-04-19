52bb80ef9cbe6735082aa3f723f76ce10ff6b9a7
Haani Niyaz <haani.niyaz@gmail.com>
2017-04-20 09:33:54 +1000

    Fix invalid index issue. Closes issue #14.

--- controllers/download.py
+++ controllers/download.py
@@ -7,36 +7,38 @@ class DownloadController(object):
 
     def __init__(self, config, log):
         self.temp_dir = config['temp_dir']
-        self.downloads = config['downloads']
+        self.downloads = self._get_active_elements(config['downloads'])
         self.log = log
         self.files_downloaded = []
 
-    def count_active_download_items(self, download_list):
-        count = 0
-        for items in download_list:
-            for val in items.itervalues():
-                if val:
-                    count += 1
-        return count
+    def _get_active_elements(self, list_dicts):
+        """ Store dict objects where value is not None 
+            Returns a dictionary 
+        """
+        active_elements = {}
+        for dict_obj in list_dicts:
+            for key, value in dict_obj.iteritems():
+                if value:
+                    active_elements[key] = value
+
+        return active_elements
 
     def download_files(self):
-        count = self.count_active_download_items(self.downloads)
 
-        for index, download_metadata in enumerate(self.downloads):
-            for link in download_metadata.itervalues():
-                if link:
-                    self.log.info("Downloading %s of %s.." % (index+1, count))
-                    file_path = self.temp_dir + '/' + \
-                        admin_tasks.get_filename(link)
-                    admin_tasks.download(link, self.temp_dir)
-                    self.files_downloaded.append(file_path)
-                    if self.log:
-                        self.log.info("File details: %s " %
-                                      admin_tasks.get_file_details(file_path))
+        total = len(self.downloads)
+
+        for index, link in enumerate(self.downloads.itervalues()):
+            self.log.info("Downloading %s of %s.." % (index+1, total))
+            file_path = self.temp_dir + '/' + \
+                admin_tasks.get_filename(link)
+            admin_tasks.download(link, self.temp_dir)
+            self.files_downloaded.append(file_path)
+            if self.log:
+                self.log.info("File details: %s " %
+                              admin_tasks.get_file_details(file_path))
 
     def summary(self):
         self.log.debug("-- Download Summary --")
-        count = self.count_active_download_items(self.downloads)
         for file_path in self.files_downloaded:
             self.log.info(admin_tasks.get_file_details(file_path))
 
