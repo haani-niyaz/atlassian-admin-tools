d650e3bb22994d1f20207dc8f2a1c7887fceecc4
Haani Niyaz <haani.niyaz@gmail.com>
2017-04-22 10:21:38 +1000

    Use dict instead of list of dicts

--- controllers/download.py
+++ controllers/download.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from utils import admin_tasks
+import sys
 
 
 class DownloadController(object):
@@ -11,31 +12,30 @@ class DownloadController(object):
         self.log = log
         self.files_downloaded = []
 
-    def _get_active_elements(self, list_dicts):
-        """ Store dict objects where value is not None 
-            Returns a dictionary 
-        """
+    def _get_active_elements(self, elements):
+        """Returns a dict of elements where value is not None"""
         active_elements = {}
-        for dict_obj in list_dicts:
-            for key, value in dict_obj.iteritems():
-                if value:
-                    active_elements[key] = value
+        for key, value in elements.iteritems():
+            if value:
+                active_elements[key] = value
 
         return active_elements
 
     def download_files(self):
 
+
         total = len(self.downloads)
 
         for index, link in enumerate(self.downloads.itervalues()):
             self.log.info("Downloading %s of %s.." % (index+1, total))
-            file_path = self.temp_dir + '/' + \
-                admin_tasks.get_filename(link)
-            admin_tasks.download(link, self.temp_dir)
-            self.files_downloaded.append(file_path)
-            if self.log:
+            file_path = self.temp_dir + '/' + admin_tasks.get_filename(link)
+            if admin_tasks.download(link, self.temp_dir):
+                self.files_downloaded.append(file_path)
                 self.log.info("File details: %s " %
-                              admin_tasks.get_file_details(file_path))
+                          admin_tasks.get_file_details(file_path))
+            else:
+                sys.exit(1)
+
 
     def summary(self):
         self.log.debug("-- Download Summary --")
