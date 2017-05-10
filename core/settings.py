a864ecdde28e9a6a8d781347b4d600b156d23718
Haani Niyaz <haani.niyaz@gmail.com>
2017-05-10 19:59:38 +1000

    Format code

--- core/settings.py
+++ core/settings.py
@@ -9,17 +9,19 @@ class ConfigFileError(Exception):
     """An error that occurs when reading a config file"""
     pass
 
+
 def read_config_file(file):
     """Returns a dictionary of objects created from a JSON config file"""
 
     if not os.path.exists(file):
         raise ConfigFileError("Ensure config file path is valid")
-        # Nesting required to use finally prior to python 2.5 
+        # Nesting required to use finally prior to python 2.5
     try:
         try:
-            fh = open(file, 'r')   
+            fh = open(file, 'r')
             return json.load(fh)
         except ValueError, e:
-            raise ConfigFileError("Configuration file has an error. %s while attempting to open %s" % (str(e),file))
+            raise ConfigFileError(
+                "Configuration file has an error. %s while attempting to open %s" % (str(e), file))
     finally:
         fh.close()
