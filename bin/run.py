54fee863d5d873832cb64c4ed9607b832b1918e1
Haani Niyaz <haani.niyaz@gmail.com>
2017-05-11 17:15:01 +1000

    Format code

--- bin/run.py
+++ bin/run.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+
 if os.getuid() != 0:
     print("Invalid usage. Script must be executed as a privileged user i.e: sudo ./run.py <options>")
     sys.exit(1)
@@ -14,4 +15,4 @@ sys.path.append(
 import setup
 
 if __name__ == '__main__':
-	setup.invoke()
+    setup.invoke()
